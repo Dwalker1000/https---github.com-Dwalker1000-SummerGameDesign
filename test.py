e01dea428c2d2eb119edf2beb886fdd8dbf6d1e3
Dwalker1000 <25walkerd@gmail.com>
2022-06-01 11:43:35 -0500

    h

---
+++ test.py
@@ -0,0 +1,19 @@
+import os
+os.system('cls')
+
+int1 = 26
+int2 = 30
+sub = int2-int1
+
+print(int2-int1)
+print(sub)
+
+print("The first number is:", int1, end = ' ')
+print("The second number is:", int2)
+
+get = input("pick a number: ")
+print(type(get))
+get = int(get)
+print(type(get))
+print(get)
+print(get + int1)
