202cb1fe8fa7a80d574397ce67e2b027167a6816
Dwalker1000 <25walkerd@gmail.com>
2022-06-03 19:36:49 -0500

    update

---
+++ IntegersNFloats.py
@@ -0,0 +1,15 @@
+import os
+import random
+os.system('cls')
+
+num = int(input("Enter a number: "))
+if (num%2 == 0):
+    print("even")
+else:
+    print("odd")
+
+if num%3==0:
+    print("multpiple of 3")
+
+if num%5==0:
+    print("multpiple of 5")
--- test.py
+++ test.py
@@ -1,17 +1,2 @@
 import os
-import random
 os.system('cls')
-
-print("===================")
-print("|       Title     |")
-print("| Instructions1   |")
-print("| Instructions2   |")
-print("===================")
-
-fruit = ["Apple", "Apricot", "Avocado", "Banana", "Blackberry", "Blueberry", "Cherry", "Coconut", "Cucumber", "Durian"]
-user = input("Guess a fruit: ")
-randy = random.choice(fruit)
-if user.lower() == randy.lower():
-    print("you win")
-else:
-    print("you louse")
